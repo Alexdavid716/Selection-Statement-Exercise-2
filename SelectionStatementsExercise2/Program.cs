19bbc82f721d9113e36081013d4f73ea18519e72
Alex David <alexdavid716@gmail.com>
2024-02-20 15:08:55 -0500

    Switch Statements

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is a tough subject!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is a tough subject!");
+                    break;
+                case "english":
+                    Console.WriteLine("English is an interesting subject!");
+                    break;
+                default:
+                    Console.WriteLine($"Oh wow! I havent taken that subject before. {subject} sounds fun!");
+                    break;
+            }
         }
     }
 }
